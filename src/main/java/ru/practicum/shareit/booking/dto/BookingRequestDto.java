170c4c26cc8119237cce09a101341ca33783d7ad
Serafim <mr.d0c@yandex.ru>
2025-10-07 16:54:15 +0300

    оптимизировать запросы и маппинг

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookingRequestDto {
+  @NotNull
   private Long itemId;
+
+  @NotNull
   private LocalDateTime start;
+
+  @NotNull
   private LocalDateTime end;
 }
