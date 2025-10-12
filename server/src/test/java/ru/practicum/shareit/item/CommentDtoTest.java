669469a4bbdd8a8a95d4242913956a3df722049b
Serafim <mr.d0c@yandex.ru>
2025-10-13 01:12:22 +0300

    Добавить запросы

--- server/src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
+++ server/src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import ru.practicum.shareit.item.dto.CommentDto;
-import ru.practicum.shareit.item.model.Item;
 
 import java.time.LocalDateTime;
 
@@ -14,37 +13,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 @JsonTest
 class CommentDtoTest {
 
-    @Autowired
-    private JacksonTester<CommentDto> json;
+  @Autowired
+  private JacksonTester<CommentDto> json;
 
-    @Test
-    void testSerialize() throws Exception {
-        CommentDto dto = new CommentDto(10L, "text_test", "Alice", LocalDateTime.of(2024, 1, 1, 10, 0));
+  @Test
+  void testSerialize() throws Exception {
+    CommentDto dto = new CommentDto(10L, "text_test", "Alice", LocalDateTime.of(2024, 1, 1, 10, 0));
 
-        assertThat(json.write(dto))
-                .extractingJsonPathNumberValue("$.id").isEqualTo(10);
-        assertThat(json.write(dto))
-                .extractingJsonPathStringValue("$.text").isEqualTo("text_test");
-        assertThat(json.write(dto))
-                .extractingJsonPathStringValue("$.authorName").isEqualTo("Alice");
-        assertThat(json.write(dto))
-                .extractingJsonPathStringValue("$.created").isEqualTo("2024-01-01T10:00:00");
-    }
+    assertThat(json.write(dto))
+            .extractingJsonPathNumberValue("$.id").isEqualTo(10);
+    assertThat(json.write(dto))
+            .extractingJsonPathStringValue("$.text").isEqualTo("text_test");
+    assertThat(json.write(dto))
+            .extractingJsonPathStringValue("$.authorName").isEqualTo("Alice");
+    assertThat(json.write(dto))
+            .extractingJsonPathStringValue("$.created").isEqualTo("2024-01-01T10:00:00");
+  }
 
-    @Test
-    void testDeserialize() throws Exception {
-        String content = "{" +
-                "\"id\": 10," +
-                "\"text\": \"text_test\"," +
-                "\"authorName\": \"Bob\"," +
-                "\"created\": \"2024-01-01T10:00:00\"" +
-                "}";
+  @Test
+  void testDeserialize() throws Exception {
+    String content = "{" +
+            "\"id\": 10," +
+            "\"text\": \"text_test\"," +
+            "\"authorName\": \"Bob\"," +
+            "\"created\": \"2024-01-01T10:00:00\"" +
+            "}";
 
-        CommentDto dto = json.parseObject(content);
+    CommentDto dto = json.parseObject(content);
 
-        assertThat(dto.getId()).isEqualTo(10);
-        assertThat(dto.getText()).isEqualTo("text_test");
-        assertThat(dto.getAuthorName()).isEqualTo("Bob");
-        assertThat(dto.getCreated()).isEqualTo(LocalDateTime.of(2024, 1, 1, 10, 0));
-    }
+    assertThat(dto.getId()).isEqualTo(10);
+    assertThat(dto.getText()).isEqualTo("text_test");
+    assertThat(dto.getAuthorName()).isEqualTo("Bob");
+    assertThat(dto.getCreated()).isEqualTo(LocalDateTime.of(2024, 1, 1, 10, 0));
+  }
 }
